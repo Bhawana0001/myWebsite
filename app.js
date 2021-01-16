677b149336f0607bc21e424516bab5741ac3b6a2
Bhawana001 <bhawana.ece18@itbhu.ac.in>
2021-01-17 00:07:19 +0530

    changes done

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-alert("This is my first website.\nIt also include some workinng button so you can website my github account and see my work.");
-=======
-alert("This is my first website.\nIt also include some workinng button so you can website my github account and see my work.");
->>>>>>> ae3bcd899b334c0e3ad62c98864db120fafca06c
+
+console.log("This is my first website.\nIt also include some workinng button so you can website my github account and see my work.");
+
